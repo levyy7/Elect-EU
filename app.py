2e99f8c717110a528c90a34a22b1d9f9e9d1e7e0
Alexandru Nistor <a.nistor@student.tudelft.nl>
2024-10-08 21:32:01 +0200

    Use HTTPS only when running locally bacause we cannot store secrets in GitLan

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from app import app
+import os
 
-if __name__ == "__main__":
-    app.run(ssl_context=("cert.pem", "key-no-pass.pem"), host="0.0.0.0", port=5000)
+cert_file = "/certs/localhost+2.pem"
+key_file = "/certs/localhost+2-key.pem"
+
+# Check if the certificate and key files exist
+if os.path.exists(cert_file) and os.path.exists(key_file):
+    # If both exist, run with SSL context (HTTPS)
+    print("Running with SSL context")
+    app.run(ssl_context=(cert_file, key_file), host="0.0.0.0", port=5000)
+else:
+    # If the files don't exist, run without SSL (HTTP)
+    print("Running without SSL context")
+    app.run(host="0.0.0.0", port=5000)
