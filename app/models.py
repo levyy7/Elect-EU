96d7c9b6923b010c766b9b505d636c0f9c642e84
Ricardo Cavalini <rcavalini@student.tudelft.nl>
2024-10-09 16:45:00 +0200

    2 different ways to login, one with a token and one with a password etc

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
+import bcrypt 
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = self.hash_password(password)
+
+    def hash_password(self, password):
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def to_json(self):
+        return {"username": self.username}
+
+
 class Vote:
     def __init__(self, user_id, vote_id):
         self.user_id = user_id
--- app/services.py
+++ app/services.py
@@ -1,5 +1,8 @@
+import jwt
+import datetime
+
 from app import mongo
-from app.models import Vote
+from app.models import Vote, User
 
 
 class VoteService:
@@ -10,3 +13,24 @@ class VoteService:
     @staticmethod
     def get_all_votes():
         return list(mongo.db.votes.find({}, {"_id": 0}))
+
+class AuthService:
+    SECRET_KEY = 'test123'
+
+    @staticmethod
+    def create_token(user: User):
+        payload = {
+            "username": user.username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }
+        return jwt.encode(payload, AuthService.SECRET_KEY, algorithm="HS256")
+
+    @staticmethod
+    def verify_token(token):
+        try:
+            payload = jwt.decode(token, AuthService.SECRET_KEY, algorithms=["HS256"])
+            return payload["username"]
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
