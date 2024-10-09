2d8fe59392eae578afb2b6a998209e6ed8928efc
Ricardo Cavalini <rcavalini@student.tudelft.nl>
2024-10-09 14:12:06 +0200

    change format to send into function

--- app/tests/unit/test_models.py
+++ app/tests/unit/test_models.py
@@ -22,8 +22,17 @@ class TestVoteModel(unittest.TestCase):
         vote = Vote(user_id="1", vote_id="123")
         expected_json = {"user_id": "1", "vote_id": "123"}
         encrypted_json = vote.encrypt_json()
+
+        # Ensure the encrypted JSON is not equal to the expected JSON
         self.assertNotEqual(encrypted_json, expected_json)
-        self.assertEqual(vote.decrypt_data(encrypted_json), expected_json)
+
+        # Decrypt the encrypted JSON back to the original format
+        decrypted_user_id = vote.decrypt_data(encrypted_json["user_id"])
+        decrypted_vote_id = vote.decrypt_data(encrypted_json["vote_id"])
+
+        # Assert
+        self.assertEqual(decrypted_user_id, expected_json["user_id"])
+        self.assertEqual(decrypted_vote_id, expected_json["vote_id"])
 
 
 if __name__ == "__main__":
