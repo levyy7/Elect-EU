3078e374aae1f2a20177694a22b67e0a70b896b2
Ricardo Cavalini <rcavalini@student.tudelft.nl>
2024-10-26 23:29:05 +0200

    try bool True

--- app/authentication_app/application/tests/integration/test_routes.py
+++ app/authentication_app/application/tests/integration/test_routes.py
@@ -23,7 +23,7 @@ def mock_authentication_service(mocker):
 # Test registration route
 def test_register_success(client, mock_authentication_service):
     mock_authentication_service.check_credentials.return_value = True
-    mock_authentication_service.generate_2fa.return_value = 123
+    mock_authentication_service.generate_2fa.return_value = True
 
     response = client.post(
         "/register",
@@ -35,7 +35,7 @@ def test_register_success(client, mock_authentication_service):
     )
     correct_responce = {
         "message": "Registration successful, scan the QR code in Google Authenticator",
-        "secret": 123,
+        "secret": True,
     }
 
     assert response.status_code == 201
