0862a746fcbe61a15c99d364a1a69a5d885ca9c9
Ricardo Cavalini <rcavalini@student.tudelft.nl>
2024-11-03 12:23:48 +0100

    test

--- app/authentication_app/application/tests/integration/test_routes.py
+++ app/authentication_app/application/tests/integration/test_routes.py
@@ -24,6 +24,7 @@ def mock_authentication_service(mocker):
 # Test registration route
 def test_register_success(client, mock_authentication_service):
     mock_authentication_service.check_credentials.return_value = True
+    mock_authentication_service.generate_2fa.return_value = 123321
 
     response = client.post(
         "/register",
