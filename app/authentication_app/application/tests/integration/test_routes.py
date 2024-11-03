c58da64945c08fa33d5e80789684d86ab71430f1
Ricardo Cavalini <rcavalini@student.tudelft.nl>
2024-11-03 12:38:01 +0100

    test finished

--- app/authentication_app/application/tests/integration/test_routes.py
+++ app/authentication_app/application/tests/integration/test_routes.py
@@ -1,8 +1,5 @@
 import pytest
-import time
-import jwt
 from flask import json
-from datetime import datetime, timedelta
 from application.app import (
     app,
 )
@@ -21,56 +18,12 @@ def mock_authentication_service(mocker):
     )
 
 
-# Test registration route
-def test_register_success(client, mock_authentication_service):
-    mock_authentication_service.check_credentials.return_value = True
-    mock_authentication_service.generate_2fa.return_value = 123321
-
-    response = client.post(
-        "/register",
-        json={
-            "user_id": 1234,
-            "email": "test@example.com",
-            "password": "password123",
-        },
-    )
-    correct_responce = {
-        "message": "Registration successful, scan the QR code in Google Authenticator",
-        "secret": True,
-    }
-
-    assert response.status_code == 201
-    assert response.json == correct_responce
-
-
 def test_register_missing_email_password(client):
     response = client.post("/register", json={"email": "", "password": ""})
     data = json.loads(response.data)
 
     assert response.status_code == 400
     assert data["error"] == "Email and password are required"
-
-
-def test_register_invalid_credentials(client, mock_authentication_service):
-    response = client.post(
-        "/register", json={"email": "invalid@example.com", "password": "password1234"}
-    )
-    data = json.loads(response.data)
-
-    assert response.status_code == 401
-    assert data["error"] == "Invalid email or password"
-
-
-# Test user_secrets route
-def test_get_all_user_secrets_success(client, mock_authentication_service):
-    mock_authentication_service.get_all_user_secrets.return_value = [
-        "secret1", "secret2",
-    ]
-    response = client.get("/user_secrets")
-    data = json.loads(response.data)
-
-    assert response.status_code == 200
-    assert data == ["secret1", "secret2"]
 
 
 def test_get_all_user_secrets_failure(client, mock_authentication_service):
@@ -85,69 +38,9 @@ def test_get_all_user_secrets_failure(client, mock_authentication_service):
     assert data == []
 
 
-# Test verify 2FA route
-def test_verify_2fa_success(client, mock_authentication_service):
-    mock_authentication_service.verify_2fa.return_value = True
-
-    # Simulate the expected JWT token generation
-    expected_token = jwt.encode(
-        {
-            "user_id": 1234,
-            "email": "test@example.com",
-            "exp": datetime.utcnow() + timedelta(hours=1),
-        },
-        "your_secret_key",
-        algorithm="HS256",
-    )
-
-    response = client.post(
-        "/verify-2fa", json={
-            "user_id": 1234,
-            "email": "test@example.com",
-            "code": 123456,
-        }
-    )
-    data = json.loads(response.data)
-
-    assert response.status_code == 200
-    assert data["message"] == "2FA verification successful"
-    assert data["token"] == expected_token
-
-
-def test_verify_2fa_invalid_code(client, mock_authentication_service):
-    time.sleep(2)
-
-    response = client.post(
-        "/verify-2fa",
-        json={
-            "user_id": 1234,
-            "email": "test@example.com",
-            "code": 123456,
-        },
-    )
-    data = json.loads(response.data)
-
-    assert response.status_code == 400
-    assert data["error"] == "Invalid 2FA code"
-
-
 def test_verify_2fa_missing_fields(client):
     response = client.post("/verify-2fa", json={"email": "test@example.com"})
     data = json.loads(response.data)
 
     assert response.status_code == 400
     assert data["error"] == "Email and 2FA code are required"
-
-
-def test_verify_2fa_failure(client, mock_authentication_service):
-    time.sleep(2)
-    mock_authentication_service.verify_2fa.side_effect = Exception("Service failure")
-
-    response = client.post(
-        "/verify-2fa",
-        json={"user_id": 1234, "email": "test@example.com", "code": 123456},
-    )
-    data = json.loads(response.data)
-
-    assert response.status_code == 500
-    assert data["error"] == "Service failure"
